4f68ee5ee925982d4b3d9d0bb87b8d9eaf93100c
abinash.p <abinash.p@mindfiresolutions.com>
2024-02-08 13:48:24 +0530

    Interface Added

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+/**
+ * @title ERC20 interface
+ * @dev see https://eips.ethereum.org/EIPS/eip-20
+ */
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
