81e3a29a82c24800ee8472d736187f25626aa2a2
abinash.p <abinash.p@mindfiresolutions.com>
2024-02-09 13:40:52 +0530

    Updated Config File

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,39 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const fs = require("fs");
+let mnemonic = fs.readFileSync(".secret").toString().trim();
+let infuraProjectID = fs.readFileSync(".infura").toString().trim();
+
+/** 
+ * @type import('hardhat/config').HardhatUserConfig 
+ **/
+
 module.exports = {
-  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + infuraProjectID,
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + infuraProjectID,
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
+  },
+  etherscan:{
+    apiKey: fs.readFileSync(".etherscan").toString().trim(),
+  },
+  sourcify: {
+    enabled: false
+  },  
+  solidity: "0.8.20",
 };
