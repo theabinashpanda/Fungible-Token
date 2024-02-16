4c5c59ad13f651ad553e530eb1aad14302f2c8ad
abinash.p <abinash.p@mindfiresolutions.com>
2024-02-16 16:10:15 +0530

    Layout changes

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -12,17 +12,20 @@ pragma solidity ^0.8.18;
 interface IERC20 {
 
     /**
-     * @dev Returns the total token supply.
+     * @dev Emitted when tokens are transferred from one account to another.
+     * @param from The address of the sender.
+     * @param to The address of the recipient.
+     * @param value The amount of tokens transferred.
      */
-
-    function totalSupply() external view returns (uint256);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     /**
-     * @dev Returns the balance of the specified account.
-     * @param account The address of the account to query the balance of.
+     * @dev Emitted when the allowance of a spender for an owner is set by the owner.
+     * @param owner The address of the owner.
+     * @param spender The address of the spender.
+     * @param value The amount of tokens allowed.
      */
-
-    function balanceOf(address account) external view returns (uint256);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     /**
      * @dev Transfers tokens from the caller's account to another account.
@@ -30,16 +33,7 @@ interface IERC20 {
      * @param amount The amount of tokens to transfer.
      * @return A boolean indicating whether the transfer was successful or not.
      */
-
     function transfer(address recipient, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that the spender is allowed to spend on behalf of the owner.
-     * @param owner The address of the owner.
-     * @param spender The address of the spender.
-     */
-
-    function allowance(address owner, address spender) external view returns (uint256);
 
     /**
      * @dev Sets the allowance for a spender.
@@ -47,7 +41,6 @@ interface IERC20 {
      * @param amount The amount of tokens to be approved.
      * @return A boolean indicating whether the approval was successful or not.
      */
-
     function approve(address spender, uint256 amount) external returns (bool);
 
     /**
@@ -57,25 +50,24 @@ interface IERC20 {
      * @param amount The amount of tokens to transfer.
      * @return A boolean indicating whether the transfer was successful or not.
      */
-
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
     /**
-     * @dev Emitted when tokens are transferred from one account to another.
-     * @param from The address of the sender.
-     * @param to The address of the recipient.
-     * @param value The amount of tokens transferred.
+     * @dev Returns the total token supply.
      */
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    function totalSupply() external view returns (uint256);
 
     /**
-     * @dev Emitted when the allowance of a spender for an owner is set by the owner.
+     * @dev Returns the balance of the specified account.
+     * @param account The address of the account to query the balance of.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Returns the remaining number of tokens that the spender is allowed to spend on behalf of the owner.
      * @param owner The address of the owner.
      * @param spender The address of the spender.
-     * @param value The amount of tokens allowed.
      */
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function allowance(address owner, address spender) external view returns (uint256);
 
 }
