438183557716cf911910bfad538e5cf192349775
abinash.p <abinash.p@mindfiresolutions.com>
2024-02-16 16:13:46 +0530

    Layout Changed and Mint Function Added

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {IERC20} from "./IERC20.sol";
+import {Owner} from "./Ownable.sol";
+
 /**
  * @title ERC20 Token Implementation
  * @dev This contract implements the ERC20 standard token functionality.
@@ -8,31 +11,13 @@ pragma solidity ^0.8.18;
  * @author https://gitlab.mindfire.co.in/abinash.p
  * @author https://github.com/theabinashpanda
  */
-import {IERC20} from "./IERC20.sol";
-
-contract ERC20Token is IERC20 {
+contract ERC20Token is IERC20,Owner {
     string private _name; // Token name
     string private _symbol; // Token symbol
-    uint256 private immutable _totalSupply; // Total token supply
-    address private _owner; // Owner of the contract
+    uint256 private _totalSupply; // Total token supply
+    uint256 private MAX_SUPPLY = 1000000; // Maximum supply
     mapping(address => uint256) private _balances; // Balances of token holders
     mapping(address => mapping(address => uint256)) private _allowances; // Allowances for token spending
-
-    /**
-     * @dev Emitted when ownership of the contract is transferred to a new address.
-     * @param previousOwner The address of the previous owner.
-     * @param newOwner The address of the new owner.
-     */
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    
-    /**
-     * @dev Modifier to only allow the owner of the contract to perform a certain action.
-     * @notice Reverts if the caller is not the owner.
-     */
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "ERC20: Only owner can perform this action");
-        _;
-    }
 
     /**
     * @dev Modifier to ensure that the specified amount is not zero.
@@ -40,6 +25,14 @@ contract ERC20Token is IERC20 {
     */
     modifier notZeroAmount(uint256 amount) {
         require(amount > 0, "ERC20: Amount should be greater than 0");
+        _;
+    }
+
+    /**
+     * @dev Modifier to ensure total supply does not exceed maximum supply.
+     */
+    modifier withinMaxSupply(uint256 amount) {
+        require(_totalSupply + amount <= MAX_SUPPLY, "ERC20: Total supply exceeds maximum supply");
         _;
     }
 
@@ -53,107 +46,13 @@ contract ERC20Token is IERC20 {
         string memory name_
         , string memory symbol_
         , uint256 initialSupply_
-    ) {
+    ) withinMaxSupply(initialSupply_) Owner(){
         require(initialSupply_ > 0,"ERC20: Value less than or equal to 0");
         _name = name_; // Set the name of the token
         _symbol = symbol_; // Set the symbol of the token
-        _owner = msg.sender; // Set the owner of the token
         _totalSupply = initialSupply_;
         _balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token.
-     */
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @dev Returns the number of decimal places for the token.
-     */
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
-    /**
-     * @dev Returns the name of the owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Returns the total supply of the token.
-     */
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev Returns the balance of the specified account.
-     * @param account The address of the account to query the balance of.
-     */
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    /**
-     * @dev Returns the allowance of a spender for a specific owner.
-     * @param from The address of the owner.
-     * @param spender The address of the spender.
-     */
-    function allowance(address from, address spender) public view returns (uint256) {
-        return _allowances[from][spender];
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new address.
-     * @param newOwner The address to transfer ownership to.
-     * @notice Only the current owner can call this function. Also, the new owner cannot be 
-     * the zero address, and the current owner cannot set the owner to themselves again.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "ERC20: new owner is the zero address");
-        require(newOwner != _owner, "ERC20: new owner is already the current owner");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-
-    /**
-     * @dev Internal function to transfer tokens from one account to another.
-     * @param sender The address of the sender.
-     * @param recipient The address of the recipient.
-     * @param amount The amount of tokens to transfer.
-     */
-    function _transfer(address sender, address recipient, uint256 amount) internal notZeroAmount(amount){
-        require(sender != recipient, "ERC20: cannot transfer to self");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _balances[sender] -= amount;
-        _balances[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-    }
-
-    /**
-     * @dev Internal function to approve a spender to spend tokens on behalf of an owner.
-     * @param from The address of the owner.
-     * @param spender The address of the spender.
-     * @param amount The amount of tokens to be approved.
-     */
-    function _approve(address from, address spender, uint256 amount) internal {
-        require(from != spender, "ERC20: cannot approve self");
-        require(spender != address(0), "ERC20: approve to the zero address");
-        _allowances[from][spender] = amount;
-        emit Approval(from, spender, amount);
     }
 
     /**
@@ -196,28 +95,98 @@ contract ERC20Token is IERC20 {
     }
 
     /**
-     * @dev Increases the allowance for a spender.
-     * @param spender The address of the spender.
-     * @param addedValue The amount of increase in allowance.
-     * @return A boolean indicating whether the increase in allowance was successful or not.
+     * @dev Mints `amount` tokens to `account`, increasing the total supply.
+     * @notice Only the owner of the contract can perform this operation.
+     * 
+     * Requirements:
+     *      `account` cannot be the zero address.
+     *      The caller cannot mint tokens to themselves.
+     *      The minted amount must be within the maximum token supply limit and cannot be 0.
+     *      
+     * @notice Emits a {Transfer} event from the zero address to account.
+     * @param account The address to which the tokens will be minted.
+     * @param amount The amount of tokens to mint.
      */
-    
-    function increaseAllowance(address spender, uint256 addedValue) public notZeroAmount(addedValue) returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
-        return true;
+    function mint(address account, uint256 amount) public onlyOwner withinMaxSupply(amount) {
+        require(account != address(0), "ERC20: Cannot mint to zero address");
+        require(msg.sender != account, "ERC20: Cannot mint to self");
+        require(amount > 0, "ERC20: Amount cannot be 0");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
     }
 
     /**
-     * @dev Decreases the allowance for a spender.
-     * @param spender The address of the spender.
-     * @param subtractedValue The amount of decrease in allowance.
-     * @return A boolean indicating whether the decrease in allowance was successful or not.
+     * @dev Internal function to transfer tokens from one account to another.
+     * @param sender The address of the sender.
+     * @param recipient The address of the recipient.
+     * @param amount The amount of tokens to transfer.
      */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public notZeroAmount(subtractedValue) returns (bool) {
-        uint256 currentAllowance = _allowances[msg.sender][spender];
-        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
-        _approve(msg.sender, spender, currentAllowance - subtractedValue);
-        return true;
+    function _transfer(address sender, address recipient, uint256 amount) internal notZeroAmount(amount){
+        require(sender != recipient, "ERC20: cannot transfer to self");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /**
+     * @dev Internal function to approve a spender to spend tokens on behalf of an owner.
+     * @param from The address of the owner.
+     * @param spender The address of the spender.
+     * @param amount The amount of tokens to be approved.
+     */
+    function _approve(address from, address spender, uint256 amount) internal {
+        require(from != spender, "ERC20: cannot approve self");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[from][spender] = amount;
+        emit Approval(from, spender, amount);
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the total supply of the token.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the balance of the specified account.
+     * @param account The address of the account to query the balance of.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev Returns the allowance of a spender for a specific owner.
+     * @param from The address of the owner.
+     * @param spender The address of the spender.
+     */
+    function allowance(address from, address spender) public view returns (uint256) {
+        return _allowances[from][spender];
+    }
+
+    /**
+     * @dev Returns the number of decimal places for the token.
+     */
+    function decimals() public pure returns (uint8) {
+        return 18;
     }
 
 }
